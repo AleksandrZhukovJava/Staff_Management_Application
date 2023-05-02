0bf724506c3e768f1ff8f9c4bf9b6dcadac3bff3
Санёчек <saska.jukoff@gmail.com>
2023-05-02 12:57:53 +0200

    Add some front end part

--- src/main/java/ru/skypro/lessons/springboot/webLibrary/controller/EmployeeController.java
+++ src/main/java/ru/skypro/lessons/springboot/webLibrary/controller/EmployeeController.java
@@ -4,31 +4,46 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.skypro.lessons.springboot.webLibrary.model.Employee;
+import org.springframework.web.servlet.ModelAndView;
 import ru.skypro.lessons.springboot.webLibrary.service.EmployeeService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 @AllArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
-
     @GetMapping("/high-salary")
-    public List<Employee> getAboveAveragePaidEmployees() {
-        return employeeService.getAboveAveragePaidEmployees();
+    public ModelAndView getAboveAveragePaidEmployees() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("employees", employeeService.getAboveAveragePaidEmployees());
+        modelAndView.setViewName("high-salary");
+        return modelAndView;
     }
     @GetMapping("/salary/max")
-    public List<Employee> getMaxSalary() {
-        return employeeService.getMaxSalaryEmployee();
+    public ModelAndView getMaxSalary() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("employees", employeeService.getMaxSalaryEmployee());
+        modelAndView.setViewName("max");
+        return modelAndView;
     }
     @GetMapping("/salary/min")
-    public List<Employee> getMinSalary() {
-        return employeeService.getMinSalaryEmployee();
+    public ModelAndView getMinSalary() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("employees", employeeService.getMinSalaryEmployee());
+        modelAndView.setViewName("min");
+        return modelAndView;
     }
     @GetMapping("/salary/sum")
-    public double getSumSalary() {
-        return employeeService.getSalarySum();
+    public ModelAndView getSumSalary() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("sum", employeeService.getSalarySum());
+        modelAndView.setViewName("sum");
+        return modelAndView;
+    }
+    @GetMapping("/salary")
+    public ModelAndView getIndexPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("main");
+        return modelAndView;
     }
 }
