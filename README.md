e6ced32a5320e3034c2a08f6b0f109ec499390c1
Санёчек <saska.jukoff@gmail.com>
2023-07-25 13:35:48 +0200

    adding README.md, refactoring test containers usage

---
+++ README.md
@@ -0,0 +1,21 @@
+# Staff Management Application
+
+Employee Management Application. Provides the ability to add, delete, edit, and view employees. Implements the functionality to gather statistical information by company departments. It can also add employees in JSON file format.
+
+
+## Security Warning
+
+**WARNING:** Application using Spring Security, you already can use it with 2 different default authorization accounts:
+1) User:1234
+2) Admin:1234 
+
+With this data you can already start to use app or add your own, In that case, don't forget to remove old data as the project is Open Source.
+
+Technology stack:
+* Spring Boot 
+* PostgreSQL 
+* JPA and Hibernate 
+*  Liquibase 
+* SLF4 and logback(with AOP)
+*  Docker 
+* Mock and JUnit
--- src/test/java/ru/skypro/lessons/springboot/webLibrary/controller/EmployeeUserControllerIntegrationTest.java
+++ src/test/java/ru/skypro/lessons/springboot/webLibrary/controller/EmployeeUserControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,10 +25,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @WithMockUser
 @Transactional
-
+@Import(PostgresqlDatabaseConnection.class)
 public class EmployeeUserControllerIntegrationTest{
-    @ClassRule
-    public static PostgresqlDatabaseConnection postgresqlDatabaseConnection = PostgresqlDatabaseConnection.getInstance();
+    @Autowired
+    public PostgresqlDatabaseConnection postgresqlDatabaseConnection;
 
     @Autowired
     private MockMvc mockMvc;
